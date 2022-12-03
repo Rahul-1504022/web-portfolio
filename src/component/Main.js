1f22a10ed663ff6b0ee7b4cf5ba906b5ea48134e
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-12-03 14:42:29 +0600

    nav added

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,11 +1,13 @@
 import About from "./body/about/About";
 import Skills from "./body/skills/Skills";
+import Nav from "./header/navbar/Nav";
 
 const Main = () => {
     return (
         <div>
+            <Nav />
             {/* <About /> */}
-            <Skills />
+            {/* <Skills /> */}
         </div>
     )
 }
---
+++ src/component/header/navbar/Nav.js
@@ -0,0 +1,14 @@
+import "./Nav.css";
+
+const Nav = () => {
+    return (
+        <div className="my_nav">
+            <img src="assets/img/my_img.jpg" alt="image" />
+            <span>About</span>
+            <span>Skills</span>
+            <span>Projects</span>
+        </div>
+    )
+}
+
+export default Nav;
