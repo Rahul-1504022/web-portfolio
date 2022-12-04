b59dd26cbcd0ad9c96663e94294c3ab3a495ca7a
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-12-04 23:06:11 +0600

    bgImage added

--- src/component/Main.js
+++ src/component/Main.js
@@ -2,14 +2,16 @@ import About from "./body/about/About";
 import Skills from "./body/skills/Skills";
 import Nav from "./header/navbar/Nav";
 import Projects from "./body/projects/Projects";
+import BgImage from "./bgimage/BgImage";
 
 const Main = () => {
     return (
         <div>
-            {/* <Nav /> */}
-            {/* <About /> */}
-            {/* <Skills /> */}
-            <Projects />
+            <BgImage />
+            {/* <Nav />
+            <About />
+            <Skills />
+            <Projects /> */}
         </div>
     )
 }
--- src/component/bgimage/BgImage.js
+++ src/component/bgimage/BgImage.js
@@ -3,7 +3,19 @@ import "../bgimage/BgImage.css";
 const BgImage = () => {
     return (
         <div className="bg">
+            {/* <img className="bg_img" src="assets/img/bg_img_2.jpg" alt="Background" /> */}
+            <div className="bg_text">
+                <div className="bg_img">
+                    <img src="assets/img/linkedin_image_1.jpg" alt="photo" width="150px" />
+                </div>
+                <h1>Md.Shahriar Mridha Rahul</h1>
+                <h3>Passionate Full Stack Web Developer</h3>
+                <div className="bg_icon">
+                    <a href="https://www.linkedin.com/in/shahriar-mridha-1088bb191/" target="_blank"><img src="assets/img/linkedin_icon.svg" alt="icon" /></a>
+                    <a href="https://github.com/Rahul-1504022" target="_blank"><img src="assets/img/github_icon_2.svg" alt="icon" /></a>
+                </div>
 
+            </div>
         </div>
     )
 }
