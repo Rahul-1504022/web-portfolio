8fbd82dde5499df02c557bf51beee4bdb5c8e4b9
Md.Shahriar Mridha Rahul <54900950+Rahul-1504022@users.noreply.github.com>
2022-12-11 21:48:17 +0600

    Footer email set

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
         <div className="footer">
             <h4>All Rights Reserved</h4>
             <h5>&copy;&nbsp;&nbsp;&nbsp;MD.SHAHRIAR MRIDHA RAHUL</h5>
+            <p>Email : <a href="mailto:shahriarmridharahul102524@gmail.com">shahriarmridharahul102524@gmail.com</a></p>
         </div>
     )
 }
